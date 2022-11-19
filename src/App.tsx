5caedef9b39d0d5ef0bb6ef23982f65c719deae8
mishanika <mihamisha68@gmail.com>
2022-11-19 20:20:44 +0200

    add left menu

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
 import React from "react";
 import "./App.css";
+import Menu from "./components/menu/Menu";
 
 const App = () => {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Menu></Menu>
+    </div>
+  );
 };
 
 export default App;
