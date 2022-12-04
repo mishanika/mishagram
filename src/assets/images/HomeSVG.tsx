fbae66f5ee4f911d8abc63c065fcf2fee5bdc0ec
mishanika <mihamisha68@gmail.com>
2022-12-04 12:33:35 +0200

    Style correction and disable selection

---
+++ src/assets/images/HomeSVG.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+const HomeSVG = () => {
+  return (
+    <svg
+      aria-label="Home"
+      className="_ab6-"
+      color="#fafafa"
+      fill="#fafafa"
+      height="24"
+      role="img"
+      viewBox="0 0 24 24"
+      width="24"
+    >
+      <path
+        d="M9.005 16.545a2.997 2.997 0 0 1 2.997-2.997A2.997 2.997 0 0 1 15 16.545V22h7V11.543L12 2 2 11.543V22h7.005Z"
+        fill="none"
+        stroke="currentColor"
+        strokeLinejoin="round"
+        strokeWidth="2"
+      ></path>
+    </svg>
+  );
+};
+
+export default HomeSVG;
