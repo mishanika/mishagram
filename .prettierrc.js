aeb2abd31920b83e12d90febb69f075c5965bc82
dpzkf <dpzkfzzz@gmail.com>
2022-12-05 00:25:14 +0200

    Аdded prettier

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  printWidth: 120,
+  useTabs: false,
+  semi: true,
+  bracketSpacing: true,
+  jsxBracketSameLine: true,
+  singleQuote: true,
+  jsxSingleQuote: false,
+  trailingComma: 'all',
+  'endOfLine': 'auto',
+};
