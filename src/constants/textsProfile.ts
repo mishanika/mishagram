92a4756e5f1b99e55c0d67daedd1d357673b9482
mishanika <mihamisha68@gmail.com>
2023-01-24 21:00:16 +0200

    add profile

---
+++ src/constants/textsProfile.ts
@@ -0,0 +1,13 @@
+export const texts = {
+  edit: 'Edit Profile',
+  posts: 'posts',
+  followers: 'followers',
+  following: 'following',
+  new: 'New',
+  saved: 'SAVED',
+  postS: 'POSTS',
+  tagged: 'TAGGED',
+  sharePhotos: 'Share photos',
+  whenYouShare: 'When you share photos, they will appear on your profile.',
+  shareFirstPhoto: 'Share your first photo',
+};
--- src/containers/profile/Profile.tsx
+++ src/containers/profile/Profile.tsx
@@ -1,7 +1,42 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
+import './Profile.css';
+import { texts } from '../../constants/textsProfile';
+import donikghoul from '../../assets/png/donikghoul.png';
+import SettingsSVG from '../../assets/svg/SettingsSVG';
 
 const Profile = () => {
-  return <h1>Page not found</h1>;
+  return (
+    <main className="profile-main-section">
+      <div className="upper-part">
+        <div className="profile-wrapper">
+          <div className="profile-photo-wrapper">
+            <img src={donikghoul} alt="" className="profile-photo" />
+          </div>
+          <div className="profile-edition-wrapper">
+            <div className="nickname-edit-wrapper">
+              <div className="nickname">mishanika2</div>
+              <div className="edit">{texts.edit}</div>
+              <div className="settings">
+                <SettingsSVG />
+              </div>
+            </div>
+            <div className="followers-wrapper">
+              <div className="posts">{texts.posts}</div>
+              <div className="folllowers">{texts.followers}</div>
+              <div className="following">{texts.following}</div>
+            </div>
+          </div>
+        </div>
+        <div className="highlights">
+          <div className="highlight">
+            <div className="highlights-circle"></div>
+            <span className="highlights-text">{texts.new}</span>
+          </div>
+        </div>
+      </div>
+      <div className="lower-part"></div>
+    </main>
+  );
 };
 
 export default Profile;
